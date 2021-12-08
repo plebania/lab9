f1b9bbd296c8f043695906c4d574757515268a2e
Marcin Scislowski <cokolwiek@c.com>
2021-12-08 16:18:54 +0100

    makefile, struktóra katalogów i pliki z wikidyd

---
+++ trunk/src/gauss.c
@@ -0,0 +1,13 @@
+#include "gauss.h"
+
+/**
+ * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ */
+int eliminate(Matrix *mat, Matrix *b){
+    /**
+     * Tutaj należy umieścić właściwą implementacje.
+     */
+
+    return 0;
+}
---
+++ trunk/src/lab9.c
@@ -0,0 +1,39 @@
+#include "gauss.h"
+#include "backsubst.h"
+#include "mat_io.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int res;
+    Matrix *A = readFromFile(argc > 1 ? argv[1] : "A.txt");
+    Matrix *b = readFromFile(argc > 2 ? argv[2] : "b.txt");
+    Matrix *x;
+
+    if (A == NULL)
+        return -1;
+    if (b == NULL)
+        return -2;
+    printToScreen(A);
+    printToScreen(b);
+
+    res = eliminate(A, b);
+    x = createMatrix(b->r, 1);
+    if (x != NULL)
+    {
+        res = backsubst(x, A, b);
+
+        printToScreen(x);
+        freeMatrix(x);
+    }
+    else
+    {
+        fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+    }
+
+    freeMatrix(A);
+    freeMatrix(b);
+
+    return 0;
+}
