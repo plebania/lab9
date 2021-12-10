151c09f99f928ed4496ae45259bfd3a801ab25a8
Tomasz Tkaczyk <ehpoprostutomek@gmail.com>
2021-12-10 16:06:08 +0100

    dodanie funkcji eliminate

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -5,7 +5,18 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b)
-{
+{ 
+
+	for(int i=1; i<mat->r; i++){
+			for(int j=0; j<mat->c; j++){
+					if(mat->data[j][j]==0)
+							return 1; 
+					double skalar= mat->data[i][j]/mat->data[j][j];
+					for(int p=0; p<mat->c; p++)
+							mat->data[i][p]-=mat->data[j][p]*skalar;
+					b->data[i][0]-=b->data[j][0]*skalar;
+			}
+	}
 
 	return 0;
 }
