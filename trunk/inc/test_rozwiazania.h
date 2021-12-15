47069a358ec172e509a388ac80013a3194783a70
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2021-12-15 08:46:50 +0100

    Poprawa testu rozwiązania oraz dodanie pliku nagłówkowego

---
+++ trunk/inc/test_rozwiazania.h
@@ -0,0 +1,7 @@
+#ifndef _TEST_ROWZWIAZANIA_H_
+#define _TEST_ROWZWIAZANIA_H_
+#include "mat_io.h"
+
+int test_rozwiazania(Matrix*, Matrix*, Matrix*);
+
+#endif // !_TEST_ROWZWIAZANIA_H_
--- trunk/src/test_rozwiazania.c
+++ trunk/src/test_rozwiazania.c
@@ -2,7 +2,10 @@
 #include "backsubst.h"
 #include "mat_io.h"
 
-void test(Matrix* x, Matrix* mat, Matrix* b) {
+int test(Matrix* x, Matrix* mat, Matrix* b) { //zwraca 1 jesli uklad nie zostal rozwiazany prawidlowo,
+                                            //zwraca 0 w przeciwnym wypadku
+
+    printf("test rozwiazania: \n");
     for (int i = 0; i < mat->r; i++) {
         double suma = 0;
         printf("%d rownanie: ", i + 1);
@@ -10,6 +13,12 @@ void test(Matrix* x, Matrix* mat, Matrix* b) {
             suma += mat->data[i][j] * x->data[j][0];
             printf("%.2lf * %.2lf + ", mat->data[i][j], x->data[j][0]);
         }
-        printf("= %.2lf, powinno byc %.2lf\n", suma, b->data[i][0]);
+        if (suma == b->data[i][0])
+            printf("= %.2lf, powinno byc %.2lf - jest okej\n", suma, b->data[i][0]);
+        else {
+            printf("= %.2lf, powinno byc %.2lf - NIE ZGADZA SIE!\n", suma, b->data[i][0]);
+            return 1;
+        }
     }
+    return 0;
 }
